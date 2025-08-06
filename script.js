fcf497cc106a3ea2519c1f24f1ed55fcf9ebb688
stefanozuffi <stefano.zuffi06@outlook.com>
2025-08-06 14:55:32 +0200

    added button to refresh the emails

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const api_url = 'https://flynn.boolean.careers/exercises/api/random/mail'
 
 const ulEl = document.querySelector('ul')
-
+const btnEl = document.querySelector('button')
 
 
 for (let i=0; i<10;i++) {
-    
-}
-axios.get(api_url)
+    axios.get(api_url)
 .then(res => {
     console.log(res);
     const thisMail = res.data.response 
@@ -19,3 +17,23 @@ axios.get(api_url)
     }
     
 })
+}
+
+function refresh() {
+    ulEl.innerHTML = ''
+    for (let i=0; i<10;i++) {
+        axios.get(api_url)
+    .then(res => {
+        console.log(res);
+        const thisMail = res.data.response 
+    
+        if (res.data.success) {
+           const liEl = document.createElement('li')
+           ulEl.insertAdjacentElement('beforeend', liEl)
+           liEl.innerText = thisMail
+        }
+    })
+    }
+}
+
+btnEl.addEventListener('click', () => refresh())
