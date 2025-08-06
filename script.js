90f7ec9712b2c0e779749ccd230f4a01486e9b4a
stefanozuffi <stefano.zuffi06@outlook.com>
2025-08-06 14:57:38 +0200

    optimization and tidiness

--- script.js
+++ script.js
@@ -4,23 +4,8 @@ const ulEl = document.querySelector('ul')
 const btnEl = document.querySelector('button')
 
 
-for (let i=0; i<10;i++) {
-    axios.get(api_url)
-.then(res => {
-    console.log(res);
-    const thisMail = res.data.response 
 
-    if (res.data.success) {
-       const liEl = document.createElement('li')
-       ulEl.insertAdjacentElement('beforeend', liEl)
-       liEl.innerText = thisMail
-    }
-    
-})
-}
-
-function refresh() {
-    ulEl.innerHTML = ''
+function addEmails() {
     for (let i=0; i<10;i++) {
         axios.get(api_url)
     .then(res => {
@@ -32,8 +17,16 @@ function refresh() {
            ulEl.insertAdjacentElement('beforeend', liEl)
            liEl.innerText = thisMail
         }
+        
     })
     }
+}
+
+addEmails()
+
+function refresh() {
+    ulEl.innerHTML = ''
+    addEmails()
 }
 
 btnEl.addEventListener('click', () => refresh())
